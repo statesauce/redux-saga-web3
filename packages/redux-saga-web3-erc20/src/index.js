7982518248f7676e56d861fe9b061e0b9e71b931
Tarrence van As <tarrence13@gmail.com>
2018-06-11 17:23:19 -0400

    ERC20 balance selector

--- packages/redux-saga-web3-erc20/src/index.js
+++ packages/redux-saga-web3-erc20/src/index.js
@@ -1,5 +1,6 @@
 import * as actions from "./erc20/actions";
+import * as selectors from "./erc20/selectors";
 import reducer from "./erc20/reducer";
 import saga from "./erc20/saga";
 
-export { actions, reducer, saga };
+export { actions, reducer, saga, selectors };
