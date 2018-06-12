585ead4eece30639954a53ef9f05f54b710b0101
Tarrence van As <tarrence13@gmail.com>
2018-06-12 17:42:55 -0400

    Allow specifying provider for block header subscription

--- packages/redux-saga-web3/src/blocks/saga.js
+++ packages/redux-saga-web3/src/blocks/saga.js
@@ -1,10 +1,11 @@
 import { getContext, takeEvery, put, take } from "redux-saga/effects";
 import { eventChannel, END } from "redux-saga";
+import Web3Eth from "web3-eth";
 
 import BLOCKS from "./types";
 
-function createEventChannel(web3) {
-  const subscription = web3.eth.subscribe("newBlockHeaders", () => {});
+function createEventChannel(eth) {
+  const subscription = eth.subscribe("newBlockHeaders", () => {});
   return eventChannel(emit => {
     subscription.on("data", data => {
       emit({
@@ -24,9 +25,18 @@ function createEventChannel(web3) {
   });
 }
 
-function* watchBlocksChannel() {
+function* watchBlocksChannel({ payload: { options } }) {
+  const { provider } = options;
   const web3 = yield getContext("web3");
-  const eventChannel = createEventChannel(web3);
+  let eventChannel;
+
+  if (provider) {
+    const eth = new Web3Eth(provider);
+    eventChannel = createEventChannel(eth);
+  } else {
+    eventChannel = createEventChannel(web3.eth);
+  }
+
   try {
     while (true) {
       var event = yield take(eventChannel);
