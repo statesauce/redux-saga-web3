c5fa4ee26cad9a2a3a3f96d91cf8c94fc26c5f4a
sethfork <sethfork@users.noreply.github.com>
2018-06-18 13:26:04 -0400

    Add action creators for ERC20 methods

--- packages/redux-saga-web3-erc20/src/erc20/actions.js
+++ packages/redux-saga-web3-erc20/src/erc20/actions.js
@@ -1,7 +1,42 @@
-import { createMethodCall } from "redux-saga-web3-eth-contract";
+import {
+  createMethodCall,
+  createMethodSend
+} from "redux-saga-web3-eth-contract";
+import {
+  CONTRACT_NAME
+} from "./constants";
 
-function balanceOf(who, options = {}) {
-  return createMethodCall("ERC20", "balanceOf", options)(who);
+// Implements actions interface for ERC20:
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+
+export function name(options = {}) {
+  return createMethodCall(CONTRACT_NAME, "name", options)();
 }
 
-export { balanceOf };
+export function symbol(options = {}) {
+  return createMethodCall(CONTRACT_NAME, "symbol", options)();
+}
+
+export function totalSupply(who, options = {}) {
+  return createMethodCall(CONTRACT_NAME, "totalSupply", options)(who);
+}
+
+export function balanceOf(owner, options = {}) {
+  return createMethodCall(CONTRACT_NAME, "balanceOf", options)(owner);
+}
+
+export function allowance(owner, spender, options = {}) {
+  return createMethodCall(CONTRACT_NAME, "allowance", options)(owner, spender);
+}
+
+export function transfer(to, tokens, options = {}) {
+  return createMethodSend(CONTRACT_NAME, "transfer", options)(to, tokens);
+}
+
+export function approve(spender, tokens, options = {}) {
+  return createMethodSend(CONTRACT_NAME, "approve", options)(spender, tokens);
+}
+
+export function transferFrom(from, to, tokens, options = {}) {
+  return createMethodSend(CONTRACT_NAME, "transferFrom", options)(from, to, tokens);
+}
