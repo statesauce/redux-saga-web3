68bfc41941ccadd7b169e16f294e848a99d64664
sethfork <sethfork@users.noreply.github.com>
2018-06-18 12:41:55 -0400

    Delete redux-web3 packages

--- packages/redux-web3-constants/src/index.js
+++
@@ -1,5 +0,0 @@
-import * as types from './actionTypes'
-import * as creators from './actionCreators'
-import * as selectors from './selectors'
-
-export { types, creators, selectors }
--- packages/redux-web3-utils/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as promisify } from "./promisify";
-export { default as getWeb3Proxy } from "./getWeb3Proxy";
-export { default as initializeContract } from "./initializeContract";
