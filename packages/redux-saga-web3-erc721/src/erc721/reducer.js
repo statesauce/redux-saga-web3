ed443571380b249de560f72184a4c61727c255c5
sethfork <sethfork@users.noreply.github.com>
2018-06-19 11:15:31 -0400

    Add metadata reductions to ERC721 reducer

--- packages/redux-saga-web3-erc721/src/erc721/reducer.js
+++ packages/redux-saga-web3-erc721/src/erc721/reducer.js
@@ -1,11 +1,8 @@
-import {
-  createTypesForMethodCall,
-  createTypesForMethodSend,
-} from "redux-saga-web3-eth-contract";
-import { CONTRACT_NAME } from "./constants";
+import {createTypesForMethodCall, createTypesForMethodSend} from "redux-saga-web3-eth-contract";
+import {CONTRACT_NAME} from "./constants";
 
 const initialState = {
-  contracts: {},
+  contracts: {}
 };
 
 const NAME = createTypesForMethodCall(CONTRACT_NAME, "name");
@@ -13,52 +10,232 @@ const SYMBOL = createTypesForMethodCall(CONTRACT_NAME, "symbol");
 const TOTAL_SUPPLY = createTypesForMethodCall(CONTRACT_NAME, "totalSupply");
 const BALANCE_OF = createTypesForMethodCall(CONTRACT_NAME, "balanceOf");
 const OWNER_OF = createTypesForMethodCall(CONTRACT_NAME, "ownerOf");
-const TOKEN_OF_OWNER_BY_INDEX = createTypesForMethodCall(
-  CONTRACT_NAME,
-  "tokenOfOwnerByIndex"
-);
+const TOKEN_OF_OWNER_BY_INDEX = createTypesForMethodCall(CONTRACT_NAME, "tokenOfOwnerByIndex");
 
 const TRANSFER = createTypesForMethodSend(CONTRACT_NAME, "transfer");
 const APPROVE = createTypesForMethodSend(CONTRACT_NAME, "approve");
 const TAKE_OWNERSHIP = createTypesForMethodSend(CONTRACT_NAME, "takeOwnership");
 
-export default (state = initialState, { type, meta, payload }) => {
+export default(state = initialState, {type, meta, payload}) => {
   switch (type) {
-    case BALANCE_OF.CALL: {
-      const {
-        options: { at },
-        args: [who],
-      } = payload;
+    case NAME.CALL:
+      {
+        const {options: {
+            at
+          }} = payload;
 
-      return {
-        ...state,
-        contracts: {
-          [at]: {
-            balances: {
-              [who]: { isLoading: true, value: null },
-            },
-          },
-        },
-      };
-    }
-    case BALANCE_OF.SUCCESS: {
-      const value = payload;
-      const {
-        options: { at },
-        args: [who],
-      } = meta;
-      return {
-        ...state,
-        contracts: {
-          [at]: {
-            balances: {
-              [who]: { isLoading: false, value },
-            },
-          },
-        },
-      };
-      return state;
-    }
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              name: {
+                isLoading: true,
+                value: null
+              }
+            }
+          }
+        }
+      }
+    case NAME.SUCCESS:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              name: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case NAME.ERROR:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              name: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case SYMBOL.CALL:
+      {
+        const {options: {
+            at
+          }} = payload;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              symbol: {
+                isLoading: true,
+                value: null
+              }
+            }
+          }
+        }
+      }
+    case SYMBOL.SUCCESS:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              symbol: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case SYMBOL.ERROR:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              symbol: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case TOTAL_SUPPLY.CALL:
+      {
+        const {options: {
+            at
+          }} = payload;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              totalSupply: {
+                isLoading: true,
+                value: null
+              }
+            }
+          }
+        }
+      }
+    case TOTAL_SUPPLY.SUCCESS:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              totalSupply: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case TOTAL_SUPPLY.ERROR:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }} = meta;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              ...state.contracts[at],
+              totalSupply: {
+                value,
+                isLoading: false
+              }
+            }
+          }
+        }
+      }
+    case BALANCE_OF.CALL:
+      {
+        const {options: {
+            at
+          }, args: [who]} = payload;
+
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              balances: {
+                [who]: {
+                  isLoading: true,
+                  value: null
+                }
+              }
+            }
+          }
+        };
+      }
+    case BALANCE_OF.SUCCESS:
+      {
+        const value = payload;
+        const {options: {
+            at
+          }, args: [who]} = meta;
+        return {
+          ...state,
+          contracts: {
+            [at]: {
+              balances: {
+                [who]: {
+                  isLoading: false,
+                  value
+                }
+              }
+            }
+          }
+        };
+        return state;
+      }
 
     default:
       return state;
