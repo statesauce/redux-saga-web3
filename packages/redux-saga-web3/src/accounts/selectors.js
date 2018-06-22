cc7454718950d62abe2946b0c6ef3921f0573bc3
sethfork <sethfork@users.noreply.github.com>
2018-06-22 13:21:09 -0400

    Migrate redux-saga-web3 to immutable.js

--- packages/redux-saga-web3/src/accounts/selectors.js
+++ packages/redux-saga-web3/src/accounts/selectors.js
@@ -1,5 +1,10 @@
+const ACCOUNTS = "accounts";
+const ITEMS = "items";
+
 const getAccounts = state =>
-  state.accounts && state.accounts.items ? state.accounts.items : null;
+  state.get(ACCOUNTS) && state.get(ACCOUNTS)[ITEMS]
+    ? state.get(ACCOUNTS)[ITEMS]
+    : null;
 
 export default {
   getAccounts,
