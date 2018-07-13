911404e139d0e98ff7934681243874c0dedb298d
Tarrence van As <tarrence13@gmail.com>
2018-07-13 12:48:15 -0400

    Cleanup ERC20

--- packages/redux-saga-web3-erc20/src/erc20/reducer.js
+++ packages/redux-saga-web3-erc20/src/erc20/reducer.js
@@ -10,37 +10,4 @@ const initialState = Map({
   contracts: Map(),
 });
 
-const BALANCE_OF = createTypesForMethodCall("ERC20", "balanceOf");
-
 export default createReducer("ERC20", abi);
-
-// export default (state = initialState, { type, meta, payload }) => {
-//   switch (type) {
-//     case BALANCE_OF.CALL: {
-//       const {
-//         options: { at },
-//         args: [who],
-//       } = payload;
-//
-//       return state.setIn(
-//         ["contracts", at, "balances", who],
-//         Map({ isLoading: true, value: null })
-//       );
-//     }
-//     case BALANCE_OF.SUCCESS: {
-//       const value = payload;
-//       const {
-//         options: { at },
-//         args: [who],
-//       } = meta;
-//
-//       return state.setIn(
-//         ["contracts", at, "balances", who],
-//         Map({ isLoading: false, value })
-//       );
-//     }
-//
-//     default:
-//       return state;
-//   }
-// };
