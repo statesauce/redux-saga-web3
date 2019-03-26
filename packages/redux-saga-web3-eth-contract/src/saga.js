cff9e45d1692ae0ba54a5e1daf912a3de78c30cb
sethfork <sethfork@users.noreply.github.com>
2019-03-26 18:05:00 -0400

    Migrate to web3@1.0.0-beta.50; Remove setProvider; add instantiateWeb3 method

--- packages/redux-saga-web3-eth-contract/src/saga.js
+++ packages/redux-saga-web3-eth-contract/src/saga.js
@@ -11,7 +11,7 @@ import {
   createTypesForEventGet,
 } from "./types";
 
-function create(namespace, contract) {
+function create(namespace, contract, methodsArr, eventsArr) {
   function getContract(options) {
     const { at, provider } = options;
     let _contract = contract.clone();
@@ -28,126 +28,121 @@ function create(namespace, contract) {
   }
 
   return () => {
-    const methods = Object.keys(contract.methods).reduce(
-      (reduction, method) => {
-        // web3 has duplicate instances of all methods
-        if (method.slice(-2) === "()" || method.substring(0, 2) === "0x") {
-          return reduction;
-        }
-        const CALL_TYPES = createTypesForMethodCall(namespace, method);
-        const SEND_TYPES = createTypesForMethodSend(namespace, method);
-        return [
-          ...reduction,
-          takeEvery(CALL_TYPES.CALL, function* send({
-            payload: { args, options },
-            meta,
-          }) {
-            const response = yield call(
-              getContract(options).methods[method](...args).call,
-              options
-            );
-            yield put({
-              type: CALL_TYPES.SUCCESS,
-              payload: response,
-              meta: {
-                ...meta,
-                args,
-                options,
-              },
-            });
-          }),
-          takeEvery(SEND_TYPES.SEND, function* watchTransactionChannel({
-            payload,
-            meta,
-          }) {
-            const { args, options } = payload;
-            if (!options.from) {
-              const account = yield select(
-                web3Selectors.accounts.selectAccounts
-              );
-              options.from = account.get(0);
-            }
-
-            function createTransactionEventChannel(args, options, meta) {
-              const emitter = getContract(options)
-                .methods[method](...args)
-                .send(options);
-
-              return eventChannel(emit => {
-                emitter.on("transactionHash", data => {
-                  emit({
-                    type: SEND_TYPES.TRANSACTION_HASH,
-                    payload: data,
-                    meta: {
-                      ...meta,
-                      args,
-                      options,
-                    },
-                  });
-                });
-                emitter.on("receipt", data =>
-                  emit({
-                    type: SEND_TYPES.RECEIPT,
-                    payload: data,
-                    meta: {
-                      ...meta,
-                      args,
-                      options,
-                    },
-                  })
-                );
-                emitter.on("confirmation", data =>
-                  emit({
-                    type: SEND_TYPES.CONFIRMATION,
-                    payload: data,
-                    meta: {
-                      ...meta,
-                      args,
-                      options,
-                    },
-                  })
-                );
-                emitter.on("error", data => {
-                  emit({
-                    type: SEND_TYPES.ERROR,
-                    payload: {
-                      name: data.name,
-                      message: data.message,
-                    },
-                    meta: {
-                      ...meta,
-                      args,
-                      options,
-                    },
-                  });
-                  emit(END);
-                });
-
-                return () => {};
-              });
-            }
-
-            const transactionEventChannel = createTransactionEventChannel(
+    const methods = methodsArr.reduce((reduction, method) => {
+      // web3 has duplicate instances of all methods
+      if (method.slice(-2) === "()" || method.substring(0, 2) === "0x") {
+        return reduction;
+      }
+      const CALL_TYPES = createTypesForMethodCall(namespace, method);
+      const SEND_TYPES = createTypesForMethodSend(namespace, method);
+      return [
+        ...reduction,
+        takeEvery(CALL_TYPES.CALL, function* send({
+          payload: { args, options },
+          meta,
+        }) {
+          const response = yield call(
+            getContract(options).methods[method](...args).call,
+            options
+          );
+          yield put({
+            type: CALL_TYPES.SUCCESS,
+            payload: response,
+            meta: {
+              ...meta,
               args,
               options,
-              meta
-            );
+            },
+          });
+        }),
+        takeEvery(SEND_TYPES.SEND, function* watchTransactionChannel({
+          payload,
+          meta,
+        }) {
+          const { args, options } = payload;
+          if (!options.from) {
+            const account = yield select(web3Selectors.accounts.selectAccounts);
+            options.from = account.get(0);
+          }
 
-            try {
-              while (true) {
-                var event = yield take(transactionEventChannel);
-                yield put(event);
-              }
-            } finally {
-              transactionEventChannel.close();
+          function createTransactionEventChannel(args, options, meta) {
+            const emitter = getContract(options)
+              .methods[method](...args)
+              .send(options);
+
+            return eventChannel(emit => {
+              emitter.on("transactionHash", data => {
+                emit({
+                  type: SEND_TYPES.TRANSACTION_HASH,
+                  payload: data,
+                  meta: {
+                    ...meta,
+                    args,
+                    options,
+                  },
+                });
+              });
+              emitter.on("receipt", data =>
+                emit({
+                  type: SEND_TYPES.RECEIPT,
+                  payload: data,
+                  meta: {
+                    ...meta,
+                    args,
+                    options,
+                  },
+                })
+              );
+              emitter.on("confirmation", data =>
+                emit({
+                  type: SEND_TYPES.CONFIRMATION,
+                  payload: data,
+                  meta: {
+                    ...meta,
+                    args,
+                    options,
+                  },
+                })
+              );
+              emitter.on("error", data => {
+                emit({
+                  type: SEND_TYPES.ERROR,
+                  payload: {
+                    name: data.name,
+                    message: data.message,
+                  },
+                  meta: {
+                    ...meta,
+                    args,
+                    options,
+                  },
+                });
+                emit(END);
+              });
+
+              return () => {};
+            });
+          }
+
+          const transactionEventChannel = createTransactionEventChannel(
+            args,
+            options,
+            meta
+          );
+
+          try {
+            while (true) {
+              var event = yield take(transactionEventChannel);
+              yield put(event);
             }
-          }),
-        ];
-      },
-      []
-    );
+          } finally {
+            transactionEventChannel.close();
+          }
+        }),
+      ];
+    }, []);
 
-    const events = Object.keys(contract.events).reduce((reduction, event) => {
+    const events = eventsArr.reduce((reduction, event) => {
       const SUBSCRIBE_TYPES = createTypesForEventSubscribe(namespace, event);
       const GET_TYPES = createTypesForEventGet(namespace, event);
       function createSubscriptionEventChannel(payload, meta) {
